818768c923ec8e3751cb42a6337c322881e2cda7
ShreyuABD06 <shreyasc.997@gmail.com>
2021-11-17 20:42:58 +0530

    Null Checks

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -14,14 +14,14 @@ const PostDetails = ({post}) => {
             <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
                 <div className="col-span-1 lg:col-span-8">
                     <PostDetail post={post}/> 
-                    <Author author={post.author}/> 
-                    <AdjacentPosts slug={post.slug} createdAt={post.createdAt} />
-                    <CommentsForm slug={post.slug}/> 
-                    <Comments slug={post.slug}/>
+                    <Author author={post?.author}/> 
+                    <AdjacentPosts slug={post?.slug} createdAt={post.createdAt} />
+                    <CommentsForm slug={post?.slug}/> 
+                    <Comments slug={post?.slug}/>
                 </div>
                 <div className="col-span-1 lg:col-span-4">
                     <div className="relative lg:sticky top-8">
-                        <PostWidget slug={post.slug} categories={post.categories.map((category) => category.slug)}/> 
+                        <PostWidget slug={post?.slug} categories={post?.categories?.map((category) => category?.slug)}/> 
                         <Categories/>
                     </div>                    
                 </div>
@@ -33,7 +33,7 @@ const PostDetails = ({post}) => {
 export default PostDetails;
 
 export async function getStaticProps({params}){
-    const data = await getPostDetails(params.slug);
+    const data = await getPostDetails(params?.slug);
     return {
       props: {post:data}
     }
